a658fdfcdf9e8b055060c288d59068f31aa82ee7
Atchin Parfait <55301545+kodak10@users.noreply.github.com>
2023-07-05 13:27:00 +0000

    Push

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,8 +6,10 @@
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
   <title>STEIL AUTOMOBILE</title>
-  <meta content="" name="description">
+  <meta content="pieces auto,reparation de vehicule,achat d'engin" name="description">
   <meta content="" name="keywords">
+  <meta name='copyright' content='steilautomobile'>
+
 
   <!-- Favicons -->
   <link href="assets/img/favicon.png" rel="icon">
